010f0945f43c0610030a8aacdf82a7785557f111
Tasssiooo <tassioribeirofontes@proton.me>
2025-01-25 22:11:47 -0300

    Done: get_deck

--- src/utils/anki_utils.py
+++ src/utils/anki_utils.py
@@ -22,7 +22,16 @@ def get_collection(path: str) -> Collection:
 # DeckId -> Deck
 # Get a DeckId and returns a Deck whose id equals DeckId.
 # If no deck is found, then it returns a new one.
-def get_deck(id): ...
+def get_deck(collection: Collection, name: str, new: bool = False):
+    deck_id = collection.decks.id(name, new)
+    if new:
+        return deck_id
+    else:
+        if deck_id:
+            return deck_id
+        else:
+            print(f'Deck "{name}" not found.')
+            sys.exit(1)
 
 
 # Str Str -> Note
@@ -37,3 +46,10 @@ def create_typein_note(front: str, back: str): ...
 
 # todo
 def create_cloze_note(text: str, back_extra: str): ...
+
+
+if __name__ == "__main__":
+    col = get_collection("/home/tassio/.local/share/Anki2/Tassio/collection.anki2")
+    print(f"get_collection: {col}")
+    deck = get_deck(col, "deck test", True)
+    print(f"get_deck: {deck}")
