8045907c449b0e6c3b46f1742d391c02476f2030
Tasssiooo <tassioribeirofontes@proton.me>
2025-04-12 21:51:45 -0300

    Improve cross-platform config directory detection

--- src/utils/configurations.py
+++ src/utils/configurations.py
@@ -2,14 +2,16 @@ import os
 import sys
 
 from configparser import ConfigParser
+from pathlib import Path
 
 
 if sys.platform == "linux" or sys.platform == "darwin":
-    config_dir = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+    config_dir = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
 else:
-    config_dir = os.environ["APPDATA"]  # Assumes it's Windows...
+    # Assumes it's Windows
+    config_dir = Path(os.environ.get("APPDATA", Path.home() / "AppData" / "Roaming"))
 
-CONFIG_FILE_PATH = os.path.join(config_dir, "lengua.conf")
+CONFIG_FILE_PATH = config_dir / "lengua.conf"
 
 
 def set_collection(collection_path: str):
@@ -17,6 +19,7 @@ def set_collection(collection_path: str):
     Writes a .conf file and sets the value 'path' of 'collection' section
     to the path to your 'collection.anki2' file.
     """
+
     config = ConfigParser()
     config.add_section("collection")
     config.set("collection", "path", collection_path)
