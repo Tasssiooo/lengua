cb78c4a7548f9764a0b586896400f847de91438c
Tasssiooo <tassioribeirofontes@proton.me>
2026-06-16 20:16:40 -0300

    Create note for Japanese content

--- src/lib/collection.py
+++ src/lib/collection.py
@@ -60,3 +60,123 @@ def create_basic_note(front: str, back: str):
 
     print("Error: note 'Basic' type is missing.")
     sys.exit(1)
+
+
+def create_japanese_note(
+    word: str,
+    word_reading: str,
+    word_meaning: str,
+    word_furigana: str,
+    word_audio: str,
+    sentence: str,
+    sentence_meaning: str,
+    sentence_furigana: str,
+    sentence_audio: str,
+    notes: str,
+    pitch_accent: str,
+    pitch_accent_notes: str,
+    frequency: str,
+    image: str,
+):
+    model = COLLECTION.models.by_name("Japanese")
+
+    if not model:
+        model = COLLECTION.models.new("Japanese")
+        template = COLLECTION.models.new_template("jp_tmpl")
+
+        template["qfmt"] = (
+            '<div lang="ja">{{Word}}<div style="font-size: 20px;">{{Sentence}}</div></div>'
+        )
+        template["afmt"] = """<div lang="ja">
+{{furigana:Word furigana}}
+
+<!-- This part enables pitch accent.
+
+{{#Pitch accent}}
+	<br><div style='font-size: 24px'>{{Pitch accent}}</div>
+{{/Pitch accent}} 
+
+-->
+
+<div style='font-size: 25px; padding-bottom:20px'>{{Word meaning}}</div>
+<div style='font-size: 25px;'>{{furigana:Sentence furigana}}</div>
+<div style='font-size: 25px; padding-bottom:10px'>{{Sentence meaning}}</div>
+
+{{Word audio}}
+{{Sentence audio}}
+<br>
+{{Image}}
+
+{{#Notes}}
+	<br>
+	<div style="font-size: 20px; padding-top:12px">Note: {{Notes}}</div>
+{{/Notes}}
+
+<!-- This part enables pitch accent notes.
+
+{{#Pitch accent notes}}
+<div style="font-size: 20px; width: fit-content; max-width:40vw; margin: auto">
+	<details><summary>Pitch Accent Notes</summary>
+		<br>{{Pitch accent notes}}
+	</details>
+</div>
+{{/Pitch accent notes}}
+
+-->
+
+</div>"""
+
+        model["flds"] = [
+            COLLECTION.models.new_field("Word"),
+            COLLECTION.models.new_field("Word reading"),
+            COLLECTION.models.new_field("Word meaning"),
+            COLLECTION.models.new_field("Word furigana"),
+            COLLECTION.models.new_field("Word audio"),
+            COLLECTION.models.new_field("Sentence"),
+            COLLECTION.models.new_field("Sentence meaning"),
+            COLLECTION.models.new_field("Sentence furigana"),
+            COLLECTION.models.new_field("Sentence audio"),
+            COLLECTION.models.new_field("Notes"),
+            COLLECTION.models.new_field("Pitch accent"),
+            COLLECTION.models.new_field("Pitch accent notes"),
+            COLLECTION.models.new_field("Frequency"),
+            COLLECTION.models.new_field("Image"),
+        ]
+        model["tmpls"] = [template]
+        model["css"] = """.card {
+ font-family: "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "Noto Sans JP", "Noto Sans CJK JP", Osaka, "メイリオ", Meiryo, "ＭＳ Ｐゴシック", "MS PGothic", "MS UI Gothic", sans-serif;
+ font-size: 44px;
+ text-align: center;
+}
+
+img {
+max-width: 300px;
+max-height: 250px;
+}
+
+.mobile img {
+max-width: 50vw;
+}
+
+/* This part defines the bold color. */
+b{color: #5586cd}"""
+
+        COLLECTION.models.add(model)
+
+    note = COLLECTION.new_note(model)
+    note["Word"] = word
+    note["Word reading"] = word_reading
+    note["Word meaning"] = word_meaning
+    note["Word furigana"] = word_furigana
+    note["Word audio"] = word_audio
+    note["Sentence"] = sentence
+    note["Sentence meaning"] = sentence_meaning
+    note["Sentence furigana"] = sentence_furigana
+    note["Sentence audio"] = sentence_audio
+    note["Notes"] = notes
+    note["Pitch accent"] = pitch_accent
+    note["Pitch accent notes"] = pitch_accent_notes
+    note["Frequency"] = frequency
+    note["Image"] = image
+
+    return model
