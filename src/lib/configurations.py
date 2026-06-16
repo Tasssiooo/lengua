014161bed31a4bd7d2ec352acc5eefb983e4e5d0
Tasssiooo <tassioribeirofontes@proton.me>
2026-06-16 20:55:58 -0300

    get_collection for ConfigParser initialization; resolve_config_file_path for cross-platform path retrieving; encoding set to utf-8.

--- src/lib/configurations.py
+++ src/lib/configurations.py
@@ -4,13 +4,33 @@ import sys
 from configparser import ConfigParser
 from pathlib import Path
 
-if sys.platform == "linux" or sys.platform == "darwin":
-    config_dir = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
-else:
-    # Assumes it's Windows
-    config_dir = Path(os.environ.get("APPDATA", Path.home() / "AppData" / "Roaming"))
+CONFIG = None
 
-CONFIG_FILE_PATH = config_dir / "lengua.conf"
+
+def resolve_config_file_path():
+    if sys.platform == "linux":
+        config_dir = Path(os.environ.get("XDG_CONFIG_HOME") or Path.home() / ".config")
+    elif sys.platform == "darwin":
+        config_dir = Path.home() / "Library" / "Application Support"
+    else:
+        config_dir = Path(
+            os.environ.get("APPDATA") or Path.home() / "AppData" / "Roaming"
+        )
+
+    config_file_path = config_dir / "lengua" / "lengua.conf"
+    config_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    return config_file_path
+
+
+def get_collection():
+    global CONFIG
+
+    if CONFIG is None:
+        CONFIG = ConfigParser()
+        CONFIG.read(resolve_config_file_path(), encoding="utf-8")
+
+    return CONFIG
 
 
 def set_collection(collection_path: str):
@@ -19,12 +39,13 @@ def set_collection(collection_path: str):
     to the path to your 'collection.anki2' file.
     """
 
-    config = ConfigParser()
+    config = get_collection()
+    config_file_path = resolve_config_file_path()
 
     if not config.has_section("collection"):
         config.add_section("collection")
 
     config.set("collection", "path", collection_path)
 
-    with open(CONFIG_FILE_PATH, "w") as lengua_conf:
+    with open(config_file_path, "w", encoding="utf-8") as lengua_conf:
         config.write(lengua_conf)
